34cfb59486e2d69fe370b45303101e0da7b45918
David Madrigal Buendía <madrigal.bd@gmail.com>
2021-01-19 13:12:44 -0600

    registro php

---
+++ forms/registrar.php
@@ -0,0 +1,12 @@
+<?php
+    include("../connection/db-connection.php");
+    $con= conection();
+    $mail= $_POST['email'];
+    $pass= $_POST['contrasenia'];
+    $name= $_POST['name'];
+    $lstname= $_POST['lastname'];
+    $tel= $_POST['phone'];
+
+    register($con, $mail, $pass, $name, $lstname, $tel);
+    echo "registro exitoso";
+?>
